99c493c4b6c3189aa5643b02c98b3c450e576a64
Alexandre Morey <alexander006_9@hotmail.com>
2021-08-12 09:34:27 +0200

    done

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,14 +1,22 @@
 // get our elements
 console.log('hello');
 const player = document.querySelector('.player');
-const video = player.querySelector('viewer');
+const video = player.querySelector('.viewer');
+
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-// console.log(video);
 // Build our fonctions
+
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  };
+};
 
 // Hook up the element listerner

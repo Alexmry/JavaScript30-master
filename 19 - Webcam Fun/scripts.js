11444b65a986a2054df8246819cdcf947c437c42
Alexandre Morey <alexander006_9@hotmail.com>
2021-08-22 10:47:58 +0200

    done

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -4,10 +4,32 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
-function getVideor() {
-  navigator.mediaDevices.getUserMedia({video: true, audio: false})
-  .then(localMediaStream => {
-    console.log(localMediaStream);
-  });
+// function getVideo() {
+//   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+//   .then(localMediaStream => {
+//     // console.log(localMediaStream);
+//     video.src = window.URL.createObjectURL(localMediaStream);
+//     video.play();
+//   })
+//   .catch(err => {
+//     console.error('you denied the webcam', err);
+//   })
+// };
 
-};
+// getVideo();
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+
+getVideo();

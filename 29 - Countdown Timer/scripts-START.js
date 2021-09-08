ad83905cac3f0cfdba65b2c8a2260fa2a7762937
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-08 11:51:44 +0200

    done

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -57,6 +57,8 @@ document.customForm.addEventListener('submit', function(e) {
   e.preventDefault();
   const mins = this.minutes.value;
   console.log(mins);
+  timer(mins * 60);
+  this.reset();
 })
 
 

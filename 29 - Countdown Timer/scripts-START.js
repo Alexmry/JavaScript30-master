f1fa01dcedc337e40732e6161f41e124c2a0c47f
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-08 09:12:19 +0200

    done

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,9 +1,23 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   // console.log({now, then});
-  setInterval(() => {
-    const secondsLeft = (then - Date.now()) / 1000;
-    console.log(secondsLeft);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    // display it
+    // console.log(secondsLeft);
+    displayTimeLeft(secondsLeft)
   }, 1000);
+};
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
 };
